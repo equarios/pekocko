7ef458bcd2ece3adfaa30eec528a9f8808ab007d
equarios <earthvi.inc@gmail.com>
2021-08-03 16:34:02 +0800

    final test

--- backend/app.js
+++ backend/app.js
@@ -21,15 +21,3 @@ app.use(morgan('combined', { stream: winston.stream }));
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/auth', userRoutes);
-app.use('/api/sauces', sauceRoutes);
-
-module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -44,11 +44,3 @@ server.on('request', () => {
     +objDate.getHours()+":"+objDate.getMinutes()+":"+objDate.getSeconds()+")");
 });
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
-
-server.listen(port);
